90646f8bfa606e62648dafa17a341129c5fddb3a
Eitan <eitanomer888@gmail.com>
2024-09-27 12:49:13 +0300

    The update:Savings_Account we created the constructors and Get-Set

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "Savings_Account"
+
---
+++ src/main/java/org/example/SavingAccount.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class SavingAccount {
+    public static double AnnualInterestRate;
+    private double SavingBalance;
+
+    public SavingAccount(double savingBalance) {
+        setSavingBalance(savingBalance);
+    }
+
+    public static double getAnnualInterestRate() {
+        return AnnualInterestRate;
+    }
+
+    public static void setAnnualInterestRate(double annualInterestRate) {
+        AnnualInterestRate = annualInterestRate;
+    }
+
+    public double getSavingBalance() {
+        return SavingBalance;
+    }
+
+    public void setSavingBalance(double savingBalance) {
+        SavingBalance = savingBalance;
+    }
+    public double CalculateMounthlyInterest(){
+        return (getAnnualInterestRate() * getSavingBalance()) / 12;
+    }
+}
